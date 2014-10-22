a8d1692eaffbefcace3a72eba9398a6b05c3c216
unknown <gvj110790@gmail.com>
2014-10-23 00:03:35 +0530

    minor change

--- src/android/nl/xservices/plugins/Toast.java
+++ src/android/nl/xservices/plugins/Toast.java
@@ -86,7 +86,7 @@ public class Toast extends CordovaPlugin {
 	
 	try {
 			//Log.d("nihar testing", "nihar action " + action);
-			JSONObject arg_object = ary.getJSONObject(0);
+			JSONObject arg_object = args.getJSONObject(0);
 			String path = arg_object.getString("filepath");
 			//Log.d("nihar testing", "nihar path " + path);
 			File file = new File(path);
