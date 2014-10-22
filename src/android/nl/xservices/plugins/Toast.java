4c02358bef15b7a0f1dd0edc81660c34171ca75f
unknown <gvj110790@gmail.com>
2014-10-22 23:23:10 +0530

    minor change

--- src/android/nl/xservices/plugins/Toast.java
+++ src/android/nl/xservices/plugins/Toast.java
@@ -5,6 +5,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
