c605b3179ba5f4b820e34e5a21bc24382e64fea6
unknown <gvj110790@gmail.com>
2014-10-23 00:17:25 +0530

    minor change

--- src/android/nl/xservices/plugins/Toast.java
+++ src/android/nl/xservices/plugins/Toast.java
@@ -87,10 +87,10 @@ public class Toast extends CordovaPlugin {
 	try {
 			//Log.d("nihar testing", "nihar action " + action);
 			JSONObject arg_object = args.getJSONObject(0);
-			//String path = arg_object.getString("filepath");
-			String path = "file:///storage/extSdCard/DCIM/Camera/20141022_104142.mp4";
+			String path = arg_object.getString("filepath");
+			//String path = "file:///storage/extSdCard/DCIM/Camera/20141022_104142.mp4";
 			//Log.d("nihar testing", "nihar path " + path);
-			File file = new File(path);
+			File file = new File(path.replace("/file:/", "file:///"));
 			byte[] bytes = null;
 			bytes = loadFile(file);
 			String encodedString = Base64.encodeToString(bytes, Base64.DEFAULT);
