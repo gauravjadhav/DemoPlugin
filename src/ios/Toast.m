2f97131fa67627f49723a73d1c27b75e75020291
gauravjadhav <revatiparandekar@Chetans-Mac-mini.local>
2014-11-05 12:37:11 +0530

    ui

--- src/ios/Toast.m
+++ src/ios/Toast.m
@@ -2,35 +2,44 @@
 #import "Toast+UIView.h"
 #import <Cordova/CDV.h>
 
+
 @implementation Toast
 
 - (void)show:(CDVInvokedUrlCommand*)command {
-
-  NSString *message  = [command.arguments objectAtIndex:0];
-  NSString *duration = [command.arguments objectAtIndex:1];
-  NSString *position = [command.arguments objectAtIndex:2];
-
-  if (![position isEqual: @"top"] && ![position isEqual: @"center"] && ![position isEqual: @"bottom"]) {
-    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"invalid position. valid options are 'top', 'center' and 'bottom'"];
+    
+    NSString *message  = [command.arguments objectAtIndex:0];
+    NSString *duration = [command.arguments objectAtIndex:1];
+    NSString *position = [command.arguments objectAtIndex:2];
+    
+    NSMutableDictionary *dict = [command.arguments objectAtIndex:0];
+    
+    NSString *stringFileName = [dict objectForKey:@"FileName"];
+    
+    if (![position isEqual: @"top"] && ![position isEqual: @"center"] && ![position isEqual: @"bottom"]) {
+        NSString *string1 = [NSString stringWithFormat:@" invalid position. valid options are 'top', 'center' and 'bottom' with filename %@", stringFileName];
+        CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:string1];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
+    
+    NSInteger durationInt;
+    if ([duration isEqual: @"short"]) {
+        durationInt = 2;
+    } else if ([duration isEqual: @"long"]) {
+        durationInt = 5;
+    } else {
+        NSString *string2 = [NSString stringWithFormat:@" invalid position. valid options are 'top', 'center' and 'bottom' with filename %@", stringFileName];
+        
+        CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:string2 ];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
+    
+    [self.webView makeToast:message duration:durationInt position:position];
+    NSString *string3 = [NSString stringWithFormat:@" plugin works with filename %@", stringFileName];
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:string3];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    return;
-  }
-
-  NSInteger durationInt;
-  if ([duration isEqual: @"short"]) {
-    durationInt = 2;
-  } else if ([duration isEqual: @"long"]) {
-    durationInt = 5;
-  } else {
-    CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"invalid duration. valid options are 'short' and 'long'"];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    return;
-  }
-
-  [self.webView makeToast:message duration:durationInt position:position];
-
-  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
