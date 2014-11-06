92a84fdfdfd8eaa7f189c73f2414c44e4c15831a
gauravjadhav <revatiparandekar@Chetans-Mac-mini.local>
2014-11-06 14:49:33 +0530

    2.49

--- src/ios/Toast.m
+++ src/ios/Toast.m
@@ -1,6 +1,19 @@
 #import "Toast.h"
 #import "Toast+UIView.h"
 #import <Cordova/CDV.h>
+#import "AFHTTPRequestOperation.h"
+#import "AFHTTPRequestOperationManager.h"
+
+
+
+typedef void (^CompletionBlock)(id object,NSError *error);
+
+
+
+#define Host_Name @ "http://8.35.33.63:8080/Api/"
+
+#define Upload @ "WorkOrderNotes/UpdateWorkOrderNotesFiles"
+
 
 
 @implementation Toast
@@ -9,29 +22,69 @@
     
   
     NSMutableDictionary *dict = [command.arguments objectAtIndex:0];
-    
-
-    
+  
     NSString* documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
     NSString* foofile = [documentsPath stringByAppendingPathComponent:[dict objectForKey:@"FilePath"]];
     BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:foofile];
+    
     NSString *stringFileName;
-    NSData *da = [NSData dataWithContentsOfFile:foofile];
-    
+    NSData *datavid = [NSData dataWithContentsOfFile:foofile];
+    NSString *base64Encoded = [datavid base64EncodedStringWithOptions:0];
 
-    if (da)
-    {
-        stringFileName = [NSString stringWithFormat:@"file was found with document path %@ and filepath %@",documentsPath,foofile];
-        
-    }
-    else
-    {
     
+    [self upload_workorderid:[dict objectForKey:@"WorkOrderId"] notetype:[dict objectForKey:@"NoteType"] sectionid:[dict objectForKey:@"SectionId"] filename:[dict objectForKey:@"FileName"] filepath:[dict objectForKey:@"FilePath"] filesize:[dict objectForKey:@"FileSize"] fileextension:[dict objectForKey:@"FilePath"] data:base64Encoded createdby:[dict objectForKey:@"CreatedBy"] surveyorid:[dict objectForKey:@"SurveyorId"] completion:^(id object, NSError *error) {
+        
+        if (object)
+        {
+            stringFileName = [NSString stringWithFormat:@"file was found with document path %@ and filepath %@",documentsPath,foofile];
+
+            
+        }
+        else
+        {
             stringFileName = [NSString stringWithFormat:@"file was not found with document path %@ and filepath %@",documentsPath,foofile];
-    }
+            
+        }
+    }];
+
     
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:stringFileName];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+
+-(void)upload_workorderid:(NSString *)workorderid notetype:(NSString *)notetype sectionid: (NSString *)sectionid filename: (NSString*) filename filepath: (NSString*) filepath filesize: (NSString*)filesize fileextension:(NSString*)fileextension data:(NSString*)data createdby:(NSString*)createdby surveyorid:(NSString*)surveyorid completion:(CompletionBlock)completion
+{
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc]init];
+    [dict setObject:workorderid forKey:@"WorkOrderId"];
+    [dict setObject:notetype forKey:@"NoteType"];
+    [dict setObject:sectionid forKey:@"SectionId"];
+    [dict setObject:filename forKey:@"FileName"];
+    [dict setObject:filepath forKey:@"FilePath"];
+    [dict setObject:filesize forKey:@"FileSize"];
+    [dict setObject:fileextension forKey:@"FileExtension"];
+    [dict setObject:data forKey:@"BinaryValue"];
+    [dict setObject:createdby forKey:@"CreatedBy"];
+    [dict setObject:surveyorid forKey:@"SurveyorId"];
+    
+    
+    NSURL *url = [NSURL URLWithString:Host_Name];
+    
+    AFHTTPRequestOperationManager *operation = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:url];
+    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    [operation POST:Upload parameters:dict success:^(AFHTTPRequestOperation *operation, id responseObject)
+     {
+         completion(responseObject,nil);
+     }
+            failure:^(AFHTTPRequestOperation *operation, NSError *error)
+     {
+         completion(nil,error);
+     }];
+}
+
+
+
+
 
 @end
